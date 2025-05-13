c2b99d7ec72ca27a50ef56497db53d57b16d98ce
Danessely <hdystasyfibkv@gmail.com>
2025-05-13 20:11:04 +0300

    Functions in preparing images

--- prepare_images_pil.py
+++ prepare_images_pil.py
@@ -19,14 +19,78 @@ from tqdm import tqdm
 
 
 INPUT_DIR = Path("data/src")
-COLOR_DIR = Path("data/frames_pil_color")
-GRAY_DIR = Path("data/frames_pil_gray")
-VIZ_DIR = Path("data/frames_pil_viz")
-MANIFEST_PATH = Path("data/frames_pil_manifest.json")
+COLOR_DIR = Path("data/frames/frames_pil_color")
+GRAY_DIR = Path("data/frames/frames_pil_gray")
+VIZ_DIR = Path("data/frames/frames_pil_scheme")
+MANIFEST_PATH = Path("data/frames/frames_pil_manifest.json")
 VARIABLE = "PWV"
 STEP_HOURS = 24  # каждые N часов (при шаге наблюдений 3 ч)
 CMAP_NAME = "jet"  # палитра для цветной карты
 VMIN, VMAX = 0, 60  # диапазон визуализации (0, ≈99-ый перцентиль)
+
+
+def to_uint8(arr, vmin=None, vmax=None):
+    """Нормируем массив -> 0...255 uint8."""
+    scaled = np.clip((arr - vmin) / (vmax - vmin), 0, 1)
+    return (scaled * 255).astype(np.uint8)
+
+def vis_color(field, ts_str):
+    gray_uint8 = to_uint8(field, VMIN, VMAX)  # (H,W) uint8
+    gray_rgb = np.dstack([gray_uint8]*3)  # (H,W,3) дублируем канал
+    gray_img = Image.fromarray(gray_rgb)
+    gray_name = f"{VARIABLE}_{ts_str}.png"
+    gray_img.save(GRAY_DIR / gray_name)
+
+def vis_gray(field, ts_str):
+    color_arr = cmap(to_uint8(field, VMIN, VMAX) / 255.0)[:, :, :3]  # drop alpha
+    color_uint8 = (color_arr * 255).astype(np.uint8)
+    color_img = Image.fromarray(color_uint8)
+    color_name = f"{VARIABLE}_{ts_str}.png"
+    color_img.save(COLOR_DIR / color_name)
+
+def vis_scheme(var, idx):
+    slice_ = var.isel(timestamp=idx)
+    fig = plt.figure(figsize=(14.4, 7.2), dpi=100)
+    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree(central_longitude=-160))
+    slice_.T.plot(
+        ax=ax,
+        transform=ccrs.PlateCarree(central_longitude=20),
+        cmap="jet",
+        vmin=VMIN,
+        vmax=VMAX,
+        add_colorbar=False,
+    )
+    # Добавление изолиний
+    levels = np.arange(VMIN, VMAX, 10)
+    contours = plt.contour(
+        slice_.lon,
+        slice_.lat,
+        slice_.T.squeeze(),
+        levels=levels,
+        colors='#222222',
+        linewidths=0.25,
+        transform=ccrs.PlateCarree(central_longitude=20)
+    )
+    plt.clabel(contours, inline=True, fontsize=8, fmt='%d')
+    ax.coastlines(linewidth=0.25)
+    ax.gridlines(draw_labels=False, linewidth=0.25, linestyle='--', color='black')
+    ax.axis('off')
+    ax.set_title("")
+    plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
+    viz_name = f"{VARIABLE}_{ts_str}.png"
+    plt.savefig(
+        VIZ_DIR / viz_name,
+        dpi=100,
+        bbox_inches=None,
+        pad_inches=0,
+        )
+    plt.close(fig)
+
+def vis_all(field, ts_str, var, idx):
+    vis_color(field, ts_str)
+    vis_gray(field, ts_str)
+    vis_scheme(var, idx)
+
 
 # создаём каталоги
 COLOR_DIR.mkdir(parents=True, exist_ok=True)
@@ -38,11 +102,6 @@ manifest = []
 
 # собираем все .nc
 nc_files = sorted(INPUT_DIR.glob("*.nc"))
-
-def to_uint8(arr, vmin=None, vmax=None):
-    """Нормируем массив -> 0...255 uint8."""
-    scaled = np.clip((arr - vmin) / (vmax - vmin), 0, 1)
-    return (scaled * 255).astype(np.uint8)
 
 for nc in nc_files:
     print(f"⏳  {nc.name}")
@@ -58,60 +117,10 @@ for nc in nc_files:
         time_val = pd.to_datetime(str(times[idx]))
         ts_str = time_val.strftime("%Y-%m-%d_%H%M")
         field = var.isel(timestamp=idx).T.values
+        filename = f"{VARIABLE}_{ts_str}.png"
+        vis_all(field, ts_str, var, idx)
 
-        # ЧБ
-        gray_uint8 = to_uint8(field, VMIN, VMAX)  # (H,W) uint8
-        gray_rgb = np.dstack([gray_uint8]*3)  # (H,W,3) дублируем канал
-        gray_img = Image.fromarray(gray_rgb)
-        gray_name = f"{VARIABLE}_{ts_str}.png"
-        gray_img.save(GRAY_DIR / gray_name)
-
-        # RGB (цветовая карта)
-        color_arr = cmap(to_uint8(field, VMIN, VMAX) / 255.0)[:, :, :3]  # drop alpha
-        color_uint8 = (color_arr * 255).astype(np.uint8)
-        color_img = Image.fromarray(color_uint8)
-        color_name = f"{VARIABLE}_{ts_str}.png"
-        color_img.save(COLOR_DIR / color_name)
-
-        # vizualizations
-        slice_ = var.isel(timestamp=idx)
-        fig = plt.figure(figsize=(14.4, 7.2), dpi=100)
-        ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree(central_longitude=-160))
-        slice_.T.plot(
-            ax=ax,
-            transform=ccrs.PlateCarree(central_longitude=20),
-            cmap="jet",
-            vmin=VMIN,
-            vmax=VMAX,
-            add_colorbar=False,
-        )
-        # Добавление изолиний
-        levels = np.arange(VMIN, VMAX, 10)
-        contours = plt.contour(
-            slice_.lon,
-            slice_.lat,
-            slice_.T.squeeze(),
-            levels=levels,
-            colors='#222222',
-            linewidths=0.25,
-            transform=ccrs.PlateCarree(central_longitude=20)
-        )
-        plt.clabel(contours, inline=True, fontsize=8, fmt='%d')
-        ax.coastlines(linewidth=0.25)
-        ax.gridlines(draw_labels=False, linewidth=0.25, linestyle='--')
-        ax.axis('off')
-        ax.set_title("")
-        plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
-        viz_name = f"{VARIABLE}_{ts_str}.png"
-        plt.savefig(
-            VIZ_DIR / viz_name,
-            dpi=100,
-            bbox_inches=None,
-            pad_inches=0,
-            )
-        plt.close(fig)
-
-        manifest.append({"data": {"image": f"data/images/data/frames_pil_color/{color_name}"}})
+        manifest.append({"data": {"image": f"data/images/data/frames/frames_pil_color/{filename}"}})
     break
 
 # сохраняем JSON
